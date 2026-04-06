37396edf9a921354050898371fa3d13cb598d430
rahulAtram <rahulatram0226@gmail.com>
2026-04-06 22:04:19 +0530

    Add server main entrypoint for validator

--- server/app.py
+++ server/app.py
@@ -143,5 +143,8 @@ def start():
     port = int(os.getenv("PORT", 7860))
     uvicorn.run("server.app:app", host="0.0.0.0", port=port, reload=False)
 
-if __name__ == "__main__":
+def main() -> None:
     start()
+
+if __name__ == "__main__":
+    main()
